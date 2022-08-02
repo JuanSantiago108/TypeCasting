4e0504f722dd7daaad478e8b43725d3484fed61b
JuanSantiago108 <santiagojuanandres@gmail.com>
2022-08-01 23:23:11 -0400

    first commit

---
+++ Program.cs
@@ -0,0 +1,17 @@
+﻿List<object> objectList = new List<object>();
+objectList.Add(7);
+objectList.Add(27);
+objectList.Add(-1);
+objectList.Add(true);
+objectList.Add("chair");
+
+int total = 0;
+
+foreach (object obj in objectList)
+if (obj is int){
+    total+=(int)obj;
+}
+
+{
+    Console.WriteLine(total);
+}
